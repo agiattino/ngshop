a9090e6b0df56005dbbc5118374f88efc6cfaff4
Giattino <40548@nelnet.net>
2021-06-14 11:36:34 -0400

    tried to add the favorite

--- src/app/dog/dog.component.ts
+++ src/app/dog/dog.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Dog } from '../dog';
-
+import { DogsService } from '../dogs.service';
+import { FavoritesService} from '../favorites.service' ;
+ 
 @Component({
   selector: 'dog',
   templateUrl: './dog.component.html',
@@ -8,10 +10,24 @@ import { Dog } from '../dog';
 })
 export class DogComponent implements OnInit {
   @Input() dog: Dog ;
+  @Input () id:any;
+  likes: number;
+  favorites: any;
   
-  constructor() { }
+  constructor(private dogService: DogsService, private favoritesService: FavoritesService) { }
 
   ngOnInit(): void {
+    this.likes = this.dogService.getLikes(this.dog.id) || 0;
+    this.favorites = this.favoritesService.all() || 0;
+  }
+
+  addLike() {
+    this.likes += 1;
+    this.dogService.update({ id: this.dog.id, likes: this.likes });
+  }
+
+  addFav() {
+    this.favoritesService.update(this.dog);
   }
 
 }
--- src/app/dogs.service.ts
+++ src/app/dogs.service.ts
@@ -4,18 +4,40 @@ import { Observable, of } from 'rxjs';
 import { Dog } from './dog';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root' ,
 })
 export class DogsService implements OnInit {
   private dogData = DOGS;
-  constructor() { }
 
-  all(): Observable<Dog[]> {
-    return of(this.dogData);
+  constructor() {
+    const likes = localStorage.getItem('likes');
+    if (!likes) {
+      localStorage.setItem('likes', JSON.stringify([]));
+   }
+  }
+
+  all(): Observable <Dog[]> {
+    return of (this.dogData)
   }
   
-  ngOnInit(): void {
-    
 
+  get(dogId: string): Dog[] {
+    return this.dogData.filter(dog => dog.id === dogId);
   }
+
+  getLikes(dogId: string): number {
+    const likes = JSON.parse(localStorage.getItem('likes'));
+    // tslint:disable-next-line:radix
+    return parseInt(likes[dogId]);
+  }
+
+  update(dog): void {
+    const likes = JSON.parse(localStorage.getItem('likes')) || localStorage.setItem('likes', JSON.stringify({}));
+    likes[dog.id] = dog.likes;
+    localStorage.setItem('likes', JSON.stringify(likes));
+  }
+  
+  ngOnInit(): void { 
+  }
+ 
 }
